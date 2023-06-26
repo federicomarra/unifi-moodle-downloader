5bf504571b07148740db960b83dde3cb2c28e896
federicomarra <fecrom@gmail.com>
2023-06-26 19:36:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# unifi-moodle-downloader
+
+> Semplice crawler e downloader di risorse per i corsi Moodle dell'Università degli Studi di Firenze.
+
+## Quick start
+
+1. ```bash
+   npm install
+   ```
+2. Aggiungi gli ID dei tuoi corsi in [`config.yaml`](config.yaml).
+3. _(Opzionale)_ Crea una copia di [`secrets.yaml`](secrets.yaml) chiamata `secrets.local.yaml`.
+4. Imposta le tue credenziali Moodle in [`secrets.local.yaml`](secrets.local.yaml) (o [`secrets.yaml`](secrets.yaml) se
+   hai saltato lo step 3).
+5. _(Opzionale)_ Crea una copia di [`path.yaml`](path.yaml) chiamata `path.local.yaml`.
+6. Imposta il tuo path (percorso) in [`path.local.yaml`](path.local.yaml) (o [`path.yaml`](path.yaml) se hai saltato lo
+   step 5). Altrimenti i file saranno scaricati ed organizzati nella cartella [`moodle-downloads`](moodle-downloader).
+7. ```bash
+   npm start
+   ```
+
+## Problemi noti
+
+I file caricati come allegati delle sezioni dei corsi vengono scaricati con i propri nome ed estensione, ma ciò non è
+ugualmente possibile per i file linkati direttamente nelle descrizioni delle sezioni: per questi ultimi non è garantità
+l'unicità del filename e vengono perciò rinominati con un prefisso incrementale: `[ALT#n] - {nome_del_file}.{ext}`.
+
+## Downloaders alternativi
+
+In caso di problemi con il downloader integrato (carico eccessivo della piattaforma Moodle, problemi di encoding...) è
+possibile impostare `config.yaml["downloader"] = "aria2"`.
+
+In questo modo i file non verranno scaricati direttamente ma verrà invece creato un file `aria2c_input.txt` che potrà
+essere passato ad [`aria2`](https://aria2.github.io/) per il download parallelo:
+> `aria2c -x 16 -j 16 -c -i ./aria2c_input.txt`
+
+## Pulizia dei file estranei
+
+Nel corso del tempo le sezioni interne dei corsi Moodle potrebbero subire delle modifiche o essere riordinate, portando
+a nuovi scaricamenti di materiale già esistente.
+
+Lo script [`cleanup_paths.sh`](cleanup_paths.sh) è in grado di eliminare localmente i documenti che su Moodle non
+esistono più o sono stati spostati, ma data l'imprevedibilità di questi cambiamenti **è sconsigliato eseguirlo
+direttamente**. Per garantire la massima integrità dei dati locali ed evitare la perdita di materiale potenzialmente
+importante ma non più disponibile online è preferibile eseguire manualmente i comandi contenuti nello script verificando
+ed adattando l'output di ognuno.
---
+++ app.js
@@ -0,0 +1,215 @@
+const fse = require('fs-extra');
+const path = require('path');
+const request = require('request-promise-native');
+const logger = require('./lib/logging').createConsoleLogger('Main');
+const Secrets = require('./lib/secrets');
+const Config = require('./lib/config');
+const {scrape} = require('./lib/scraper');
+const {sanitizePath} = require('./lib/utils');
+
+function panic(e, exit) {
+  logger.error(e);
+  if (exit) process.exit(1);
+}
+
+function removeParamsFromUrl(url) {
+  let urlParamIndex = url.indexOf('?');
+  return (urlParamIndex !== -1 && url.indexOf('=') > urlParamIndex) ? url.substring(0, urlParamIndex) : url;
+}
+/*
+
+*/
+async function prepareDownload(sessionCookie, course, section, filename, url, tryCounter) {
+  if (tryCounter === 3) {
+    return;
+  }
+
+  Config.loadPath()
+    .then((downloadPath) => {
+      const pathD = downloadPath['path'];
+      return pathD;
+    })
+    .catch(e => panic(e, false));
+
+  //const downloadPath = path.resolve(__dirname, 'downloads', course, section);
+  const downloadPath = path.resolve(Config.loadPath(), course, section);
+  fse.mkdirpSync(downloadPath);
+
+  let r;
+  try {
+    r = await request.get({
+      url: url,
+      headers: {
+        "cookie": `MoodleSession=${sessionCookie};`,
+      },
+      resolveWithFullResponse: true,
+      encoding: null,
+      timeout: 30000,
+    });
+  } catch (err) {
+    logger.error("Error in preflight filename extension extraction", err);
+    return prepareDownload(sessionCookie, course, section, filename, url, tryCounter !== undefined ? ++tryCounter : 1);
+  }
+  let uriHref = removeParamsFromUrl(r.request.uri.href);
+  let urlExtension = uriHref.substring(uriHref.lastIndexOf('.') + 1);
+
+  filename = filename.trim();
+  if (!filename.endsWith(urlExtension)) {
+    filename = `${filename}.${urlExtension}`;
+  }
+
+  return {
+    sessionCookie,
+    url,
+    filename: sanitizePath(filename),
+    path: downloadPath,
+  };
+}
+
+async function download(downloadMetadata) {
+  const downloadedFilePath = path.resolve(downloadMetadata.path, downloadMetadata.filename);
+
+  await request.get({
+    url: downloadMetadata.url,
+    headers: {
+      "cookie": `MoodleSession=${downloadMetadata.sessionCookie};`,
+    },
+    resolveWithFullResponse: true,
+    encoding: null,
+    timeout: 30000,
+  })
+    .then(r => {
+      /*
+      let uriHref = removeParamsFromUrl(r.request.uri.href);
+      let urlExtension = uriHref.substring(uriHref.lastIndexOf('.') + 1);
+      fse.writeFileSync(path.resolve(downloadMetadata.path, `${downloadMetadata.filename}.${urlExtension}`.replaceAll(path.sep, '_'))), r.body);
+      */
+      fse.writeFileSync(downloadedFilePath, r.body);
+    })
+    .catch(e => panic(e, false));
+
+  return true;
+}
+
+function aria2c(downloadMetadata) {
+  let inputFileBlockLines = [downloadMetadata.url];
+  inputFileBlockLines.push(`  header=Cookie:MoodleSession=${downloadMetadata.sessionCookie}`);
+  inputFileBlockLines.push(`  dir=${downloadMetadata.path}`);
+  inputFileBlockLines.push(`  out=${downloadMetadata.filename}`);
+  inputFileBlockLines.push("");
+  return inputFileBlockLines.join("\n");
+}
+
+Config.load()
+  .then((config) => {
+    Secrets.load()
+      .then((secrets) => {
+        scrape(secrets, config)
+          .then(async (r) => {
+            const toBeDownloaded = [];
+            const allPaths = [];
+            const aria2cInputFileBlocks = [];
+            const downloadLogPrefix = config.downloader !== "internal" ? "Preparazione download" : "Download";
+
+            function downloadIfMissing(downloadMetadata) {
+              if (downloadMetadata) {
+                if (fse.pathExistsSync(path.join(downloadMetadata.path, downloadMetadata.filename))) {
+                  logger.silly(`        └─ Risorsa ignorata, esiste già`);
+                } else {
+                  toBeDownloaded.push(downloadMetadata);
+                }
+              }
+            }
+
+            function dumpAllPathsIfSet(downloadMetadata) {
+              if (process.env['DUMP_ALL_PATHS']) {
+                allPaths.push(`${downloadMetadata.path}/${downloadMetadata.filename}`);
+              }
+            }
+
+            for (const courseId in r.contents) {
+              if (r.contents.hasOwnProperty(courseId)) {
+                logger.info(`${downloadLogPrefix} del corso con ID: ${courseId}...`);
+                let course = r.contents[courseId];
+                let courseName = course.courseName;
+                logger.debug(`└─ ${downloadLogPrefix} corso: "${courseName}"...`);
+
+                for (const sectionName in course.sections) {
+                  if (course.sections.hasOwnProperty(sectionName)) {
+                    logger.debug(`   └─ ${downloadLogPrefix} sezione: "${sectionName}"...`);
+                    const sectionFiles = course.sections[sectionName].files;
+                    const sectionFolders = course.sections[sectionName].folders;
+
+                    for (const resourceName in sectionFiles) {
+                      if (sectionFiles.hasOwnProperty(resourceName)) {
+                        let filename = `${sectionFiles[resourceName].prefix}. ${resourceName}`;
+                        let url = sectionFiles[resourceName].url;
+                        logger.debug(`      └─ Analisi risorsa: "${filename}" @ "${url}"...`);
+
+                        const downloadMetadata = await prepareDownload(r.sessionCookie, courseName, sectionName, filename, url);
+                        dumpAllPathsIfSet(downloadMetadata);
+                        downloadIfMissing(downloadMetadata);
+                      }
+                    }
+
+                    for (const folderName in sectionFolders) {
+                      if (sectionFolders.hasOwnProperty(folderName)) {
+                        const folder = sectionFolders[folderName];
+                        for (const resourceName in folder) {
+                          if (folder.hasOwnProperty(resourceName)) {
+                            let suffixedSectionName = `${sectionName}/${folderName}`;
+                            let filename = `${folder[resourceName].prefix}. ${resourceName}`;
+                            let url = folder[resourceName].url;
+                            logger.debug(`      └─ Analisi risorsa in cartella "${folderName}": "${filename}" @ "${url}"...`);
+
+                            const downloadMetadata = await prepareDownload(r.sessionCookie, courseName, suffixedSectionName, filename, url);
+                            dumpAllPathsIfSet(downloadMetadata);
+                            downloadIfMissing(downloadMetadata);
+                          }
+                        }
+                      }
+                    }
+
+                    for (const downloadMetadata of toBeDownloaded) {
+                      logger.debug(`      └─ ${downloadLogPrefix} risorsa: "${downloadMetadata.filename}" @ "${downloadMetadata.url}"...`);
+                      switch (config.downloader) {
+                        case "aria2":
+                          aria2cInputFileBlocks.push(aria2c(downloadMetadata));
+                          break;
+                        case "internal":
+                        default:
+                          logger.silly(`Download file in: ${downloadMetadata.path}${path.sep}${downloadMetadata.filename}`);
+                          const wasDownloaded = await download(downloadMetadata);
+                          if (!wasDownloaded) {
+                            logger.silly(`        └─ Download skipped! An error has occurred.`);
+                          }
+                          break;
+                      }
+                    }
+                  }
+
+                  toBeDownloaded.splice(0, toBeDownloaded.length); // Clear in place
+                }
+              }
+            }
+
+            if (config.downloader === "aria2" && aria2cInputFileBlocks.length > 0) {
+              fse.writeFileSync(path.resolve(path.join(__dirname, "aria2c_input.txt")), aria2cInputFileBlocks.join('\n'));
+              const resourcesCount = aria2cInputFileBlocks.length;
+              logger.warn(`${resourcesCount} nuov${resourcesCount > 1 ? 'e' : 'a'} risors${resourcesCount > 1 ? 'e' : 'a'} da scaricare.`);
+              logger.info("File di input per aria2c scritto in \"aria2c_input.txt\".");
+              logger.info("Esempio di comando di download: \"aria2c -x 16 -s 16 -c -i aria2c_input.txt\"");
+            } else {
+              logger.warn("Nessuna nuova risorsa da scaricare.");
+            }
+
+            if (process.env['DUMP_ALL_PATHS'] && allPaths.length > 0) {
+              logger.warn("Dump di tutti i path attuali in 'paths.txt'");
+              fse.writeFileSync(path.resolve(path.join(__dirname, "paths.txt")), allPaths.join('\n') + '\n');
+            }
+          })
+          .catch(e => panic(e, false));
+      })
+      .catch(e => panic(e, false));
+  })
+  .catch(e => panic(e, false));
